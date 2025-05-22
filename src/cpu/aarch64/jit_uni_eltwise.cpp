af6e59e80bade4ff7c86c594101214c502d2cc67
Ryo Suzuki <ryo.suzuki@arm.com>
2025-05-22 09:44:44 +0100

    cpu: aarch64: eltwise: Use predication & get rid of tail loop (#3289)

--- src/cpu/aarch64/jit_uni_eltwise.cpp
+++ src/cpu/aarch64/jit_uni_eltwise.cpp
@@ -95,11 +95,17 @@ struct jit_uni_kernel_t : public jit_uni_eltwise_kernel_t {
         ldr(reg_work_amount, ptr(X_TMP_0));
         eltwise_injector_->load_table_addr();
 
-        Label reminder_loop_start, reminder_loop_end;
+        // Predicates used for load and store operations.
+        // Initially set to ptrue until we have "< vector length"
+        // number of items to process.
+        ptrue(pg_s.s);
+        ptrue(pg_h.h);
+
+        Label tail_predication;
         Label vectorized_loop_start, vectorized_loop_end;
 
         cmp(reg_work_amount, simd_w());
-        b(LT, reminder_loop_start);
+        b(LT, tail_predication);
 
         L(vectorized_loop_start);
 
@@ -113,48 +119,52 @@ struct jit_uni_kernel_t : public jit_uni_eltwise_kernel_t {
         // there's a restriction on certain blocked layouts, when this behavior
         // can be relevantly easy controlled, this will cost much from code
         // perspective and will complicate the compute logic significantly.
-        ldr(vmm_src, ptr(reg_src));
 
         if (is_bf16()) {
+            ld1h(vmm_src.h, pg_h / T_z, ptr(reg_src));
             // Convert BF16 input to FP32, apply eltwise op, then convert back to BF16:
             // - unpack BF16 to FP32 by zero-extending
             // - compute eltwise alg in FP32
             // - down convert back to BF16 using bfcvt, and pack result
-            mov(tmp0.s, P_ALL_ONE, vmm_src.s);
+            mov(tmp0.s, pg_s, vmm_src.s);
             lsl(vmm_src.s, vmm_src.s, 16);
             and_(tmp0.s, 0xFFFF0000);
             eltwise_injector_->compute_vector_range(
                     {vmm_src.getIdx(), tmp0.getIdx()});
-            bfcvt(vmm_src.h, P_ALL_ONE, vmm_src.s);
-            bfcvtnt(vmm_src.h, P_ALL_ONE, tmp0.s);
+            bfcvt(vmm_src.h, pg_h, vmm_src.s);
+            bfcvtnt(vmm_src.h, pg_h, tmp0.s);
+            st1h(vmm_src.h, pg_h / T_z, ptr(reg_dst));
         } else if (is_f16()) {
+            ld1h(vmm_src.h, pg_h / T_z, ptr(reg_src));
             // Convert FP16 to FP32, apply eltwise op, then convert back to FP16:
             // - upcast FP16 to FP32 using fcvt
             // - compute eltwise alg in FP32
             // - downcast FP32 back to FP16 using fcvt, and pack result
-            mov(tmp0.s, P_ALL_ONE, vmm_src.s);
-            fcvt(vmm_src.s, P_ALL_ONE, vmm_src.h);
+            mov(tmp0.s, pg_s, vmm_src.s);
+            fcvt(vmm_src.s, pg_h, vmm_src.h);
             // Next two lines could be replaced by fcvtlt(tmp0.s, P_ALL_ONE, tmp0.h)
             // Not currently implemented in xbyak
             lsr(tmp0.s, tmp0.s, 16);
-            fcvt(tmp0.s, P_ALL_ONE, tmp0.h);
+            fcvt(tmp0.s, pg_h, tmp0.h);
             eltwise_injector_->compute_vector_range(
                     {vmm_src.getIdx(), tmp0.getIdx()});
-            fcvt(vmm_src.h, P_ALL_ONE, vmm_src.s);
+            fcvt(vmm_src.h, pg_s, vmm_src.s);
             // Next three lines could be replaced by fcvtnt(vmm_src.h, P_ALL_ONE, tmp0.s)
             // Not currently implemented in xbyak
-            fcvt(tmp0.h, P_ALL_ONE, tmp0.s);
+            fcvt(tmp0.h, pg_s, tmp0.s);
             lsl(tmp0.s, tmp0.s, 16);
-            orr(vmm_src.h, P_ALL_ONE, tmp0.h);
+            orr(vmm_src.h, pg_h, tmp0.h);
+            st1h(vmm_src.h, pg_h / T_z, ptr(reg_dst));
         } else {
+            ld1w(vmm_src.s, pg_s / T_z, ptr(reg_src));
             eltwise_injector_->compute_vector(vmm_src.getIdx());
+            if (!is_fwd) {
+                ld1w(ZReg(vmm_diff_dst.getIdx()).s, pg_s / T_z,
+                        ptr(reg_diff_dst));
+                fmul(vmm_src.s, vmm_src.s, vmm_diff_dst);
+            }
+            st1w(vmm_src.s, pg_s / T_z, ptr(reg_dst));
         }
-
-        if (!is_fwd) {
-            ldr(ZReg(vmm_diff_dst.getIdx()), ptr(reg_diff_dst));
-            fmul(vmm_src.s, vmm_src.s, vmm_diff_dst);
-        }
-        str(vmm_src, ptr(reg_dst));
 
         const auto shift = cpu_isa_traits<isa>::vlen;
         add_imm(reg_src, reg_src, shift, X_TMP_0);
@@ -165,52 +175,24 @@ struct jit_uni_kernel_t : public jit_uni_eltwise_kernel_t {
         cmp(reg_work_amount, simd_w());
         b(GE, vectorized_loop_start);
 
-        L(vectorized_loop_end);
-
-        L(reminder_loop_start);
+        L(tail_predication);
 
         cmp(reg_work_amount, 0);
-        b(LE, reminder_loop_end);
+        b(LE, vectorized_loop_end);
 
-        if (is_bf16()) {
-            ld1(v_bf16[0], ptr(reg_src));
-            // Convert BF16 input to FP32, apply eltwise op, then convert back to BF16
-            shll(VReg4S(v_bf16[0].getIdx()), VReg4H(v_bf16[0].getIdx()), 16);
-            eltwise_injector_->compute_vector(v_bf16.getIdx());
-            bfcvt(ZReg(v_bf16[0].getIdx()).h, P_ALL_ONE,
-                    ZReg(v_bf16[0].getIdx()).s);
-        } else if (is_f16()) {
-            ld1(v_f16[0], ptr(reg_src));
-            // Convert FP16 input to FP32, apply eltwise op, then convert back to FP16
-            fcvt(ZReg(v_f16[0].getIdx()).s, P_ALL_ONE,
-                    ZReg(v_f16[0].getIdx()).h);
-            eltwise_injector_->compute_vector(v_f16.getIdx());
-            fcvt(ZReg(v_f16[0].getIdx()).h, P_ALL_ONE,
-                    ZReg(v_f16[0].getIdx()).s);
-        } else {
-            ld1(xmm_src[0], ptr(reg_src));
-            eltwise_injector_->compute_vector(xmm_src.getIdx());
+        // Instead of a tail loop, we use SVE predication to only load
+        // the remainder elements, with the inactive elements of the vector
+        // set to 0. This is done outside of the vectorized_loop to avoid
+        // unnecessary overhead.
+        mov_imm(X_TMP_1, 0);
+        whilelt(pg_s.s, X_TMP_1, reg_work_amount);
+        if (is_bf16() || is_f16()) {
+            whilelt(pg_h.h, X_TMP_1, reg_work_amount);
         }
 
-        if (!is_fwd) {
-            ld1(xmm_diff_dst[0], ptr(reg_diff_dst));
-            fmul(xmm_src, xmm_src, xmm_diff_dst);
-        }
-        if (is_bf16()) {
-            st1(v_bf16[0], ptr(reg_dst));
-        } else if (is_f16()) {
-            st1(v_f16[0], ptr(reg_dst));
-        } else {
-            st1(xmm_src[0], ptr(reg_dst));
-        }
-        add_imm(reg_src, reg_src, dtype_size(), X_TMP_0);
-        add_imm(reg_dst, reg_dst, dtype_size(), X_TMP_0);
-        if (!is_fwd) add_imm(reg_diff_dst, reg_diff_dst, dtype_size(), X_TMP_0);
+        b(vectorized_loop_start);
 
-        subs(reg_work_amount, reg_work_amount, 1);
-        b(reminder_loop_start);
-
-        L(reminder_loop_end);
+        L(vectorized_loop_end);
 
         postamble();
 
@@ -244,6 +226,8 @@ private:
     std::unique_ptr<jit_uni_eltwise_injector_f32<isa>> eltwise_injector_;
 
     PReg p_tmp0 {4}; /* Index is temporal. */
+    PReg pg_s {5};
+    PReg pg_h {7};
 };
 
 } // namespace
