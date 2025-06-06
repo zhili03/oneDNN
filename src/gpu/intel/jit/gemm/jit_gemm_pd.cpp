9b56dbe008c44ffcfae477bbec01ff6532ea15de
Guskov, Andrey Y <andrey.y.guskov@intel.com>
2025-06-05 22:03:30 -0700

    src: gpu: intel: jit: gemm: treat dst s8/u8 as dy_quant

--- src/gpu/intel/jit/gemm/jit_gemm_pd.cpp
+++ src/gpu/intel/jit/gemm/jit_gemm_pd.cpp
@@ -182,7 +182,7 @@ bool jit_gemm_pd_t::dy_quant_enabled() {
     bool all_f8 = (utils::one_of(d->a_type(), f8_e5m2, f8_e4m3)
             && utils::one_of(d->b_type(), f8_e5m2, f8_e4m3)
             && utils::one_of(d->c_type(), f8_e5m2, f8_e4m3, f16, bf16, f32));
-    return (utils::one_of(d->c_type(), f32, f16, bf16)
+    return (utils::one_of(d->c_type(), f32, f16, bf16, u8, s8)
                    && utils::one_of(d->a_type(), u8, s8, s4, u4)
                    && utils::one_of(d->b_type(), u8, s8))
             || all_f8;
